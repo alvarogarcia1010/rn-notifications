288cc5ec49cb5720b6e3d2131100f1ba563dd8b9
alvarogarcia1010 <00111317@uca.edu.sv>
2021-03-05 08:33:03 -0600

    My firts commit

--- App.js
+++ App.js
@@ -1,11 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Notifications from 'expo-notifications'
+import * as Permissions from 'expo-permissions'
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+    }
+  }
+})
 
 export default function App() {
+
+  useEffect(() => {
+    Permissions.getAsync(Permissions.NOTIFICATIONS)
+      .then(statusObj => {
+        if(statusObj.status !== 'granted'){
+          return Permissions.askAsync(Permissions.NOTIFICATIONS)
+        }
+        return statusObj;
+      })
+      .then(statusObj => {
+        if(statusObj.status !== 'granted'){
+          return;
+        }
+      })
+  }, [])
+
+
+  useEffect(() => {
+    const backgroundSubscription = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    })
+
+    const foregroundSubscription = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification)
+    })
+
+    return () => {
+      backgroundSubscription.remove()
+      foregroundSubscription.remove()
+    }
+  }, [])
+
+  const triggerNotificationsHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is the first local notification we are sending',
+        data: {
+          mySpecialData: "Some text"
+        }
+      },
+      trigger: {
+        seconds: 10,
+      }
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Trigger Notification" onPress={triggerNotificationsHandler} />
       <StatusBar style="auto" />
     </View>
   );
